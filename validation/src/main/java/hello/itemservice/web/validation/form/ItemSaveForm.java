346256e0a89381fe5556d2a42e1e4ba97d37ae4e
jaeshim <jaeshim@gmarket.com>
2023-05-26 17:52:25 +0900

    20230526

---
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -0,0 +1,21 @@
+package hello.itemservice.web.validation.form;
+
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+public class ItemSaveForm {
+
+  @NotBlank
+  private String itemName;
+  @NotNull
+  @Range(min = 1000, max = 1000000)
+  private Integer price;
+  @NotNull
+  @Max(value = 9999) //수정 요구사항 추가
+  private Integer quantity;
+}
